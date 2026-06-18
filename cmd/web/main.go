da632f705dff9effedf6053b61985c9429daa95d
sumitpoudel <sumitpoudel@proton.me>
2026-06-18 23:57:04 +0545

    DB connecting Pool
    using seprate models package for code reusebality

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,26 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	_ "github.com/lib/pq"
+	"github.com/sumit-poudel/snippetbox/internal/models"
 )
 
 type application struct {
 	infoLog  *log.Logger
 	errorLog *log.Logger
+	snippets *models.SnippetModel
 }
 
 func main() {
 	// declare flag for the runtime
 	addr := flag.String("addr", ":4000", "Port address for the server")
+	dsn := flag.String("dsn", "host=localhost user=web password=pass dbname=snippetbox sslmode=disable", "MySQL data source name")
 	// parse the flag
 	flag.Parse()
 
@@ -22,9 +28,17 @@ func main() {
 	infoLog := log.New(os.Stdout, "INFO:\t", log.Ldate|log.Ltime)
 	errLog := log.New(os.Stderr, "ERROR: \t", log.Ldate|log.Ltime|log.Lshortfile)
 
+	db, errr := openDB(*dsn)
+
+	if errr != nil {
+		log.Fatal(errr)
+	}
+	defer db.Close()
+
 	app := &application{
 		infoLog:  infoLog,
 		errorLog: errLog,
+		snippets: &models.SnippetModel{DB: db},
 	}
 
 	srv := &http.Server{
@@ -35,6 +49,18 @@ func main() {
 	infoLog.Printf("Starting server at %s", *addr)
 	err := srv.ListenAndServe() // <-- this takes a handler
 	errLog.Fatal(err)
+}
+
+func openDB(dns string) (*sql.DB, error) {
+	db, errr := sql.Open("postgres", dns)
+	if errr != nil {
+		return nil, errr
+	}
+	if errr = db.Ping(); errr != nil {
+		return nil, errr
+	}
+
+	return db, errr
 }
 
 /*
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sumit-poudel/snippetbox
 
 go 1.26.4
+
+require github.com/lib/pq v1.12.3
